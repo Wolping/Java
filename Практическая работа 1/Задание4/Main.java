4c5a063b88a0112a0c627821efaf694f75d30a70
Wolping <90902590+Wolping@users.noreply.github.com>
2022-06-15 14:38:05 +0300

    Update Main.java

--- Практическая работа 1/Задание4/Main.java
+++ Практическая работа 1/Задание4/Main.java
@@ -1,12 +1,18 @@
-package Задание4;
+package com.company;
+import java.util.Scanner;
 public class Main {
-    public static void main (String[] args) {
-            String name = new String("Петя");
-        String password = new String("123");
-        if ( args[0].equals(name) && args[1].equals(password)) {
-            System.out.print("Вас узнали. Добро пожаловать");
-        } else {
-            System.out.print("Логин и пароль не распознаны. Доступ запрещен.");
+
+    public static void main(String[] args) {
+        Scanner str = new Scanner(System.in);
+        String login = "admen";
+        String password = "odmen";
+        String l, p;
+        System.out.println("Введите логин и пароль");
+        System.out.print("логин: "); l = str.nextLine();
+        System.out.print("пароль: "); p = str.nextLine();
+        if(l.equals(login) && p.equals(password)){System.out.println("Вас узнали. Добро пожаловать!");}
+        else {
+            System.out.println("Логин и пароль не распознаны. Доступ запрещен");
         }
     }
 }
