bf44bcf971a9eb399f02b83f8d1a1d879345ee19
Wolping <90902590+Wolping@users.noreply.github.com>
2022-06-15 16:52:55 +0300

    Update Main.java

--- Практическая работа 2/Задание4/Main.java
+++ Практическая работа 2/Задание4/Main.java
@@ -1,18 +1,25 @@
-package com.company;
-import java.util.Scanner;
-public class Main {
-
+public class pr {
     public static void main(String[] args) {
-        Scanner str = new Scanner(System.in);
-        String login = "admen";
-        String password = "odmen";
-        String l, p;
-        System.out.println("Введите логин и пароль");
-        System.out.print("логин: "); l = str.nextLine();
-        System.out.print("пароль: "); p = str.nextLine();
-        if(l.equals(login) && p.equals(password)){System.out.println("Вас узнали. Добро пожаловать!");}
-        else {
-            System.out.println("Логин и пароль не распознаны. Доступ запрещен");
+    	int u = 0;
+        int n = 5;        
+        int mat[][] = new int[n][n];
+        for (int i=0; i<n; i++)
+        {
+            for (int j=0; j<n; j++)
+            {
+                mat[i][j] = (int)((Math.random() * 20)-10);
+                System.out.print(mat[i][j] + "  ");
+            }
+            System.out.println();
         }
+        
+        for (int i=0; i<n; i++)
+        {
+             u = u + mat[i][i];
+        }
+        u=u/n;
+        System.out.println(u);
     }
 }
+
+//дан двухмерный массив n n найти среднее арифметическое положительных элементов главной диагонали
