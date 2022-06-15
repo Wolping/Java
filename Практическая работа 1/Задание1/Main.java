9b871400667098d2a3bcb06e7495020fbe85afd1
Wolping <90902590+Wolping@users.noreply.github.com>
2022-06-15 14:35:59 +0300

    Update Main.java

--- Практическая работа 1/Задание1/Main.java
+++ Практическая работа 1/Задание1/Main.java
@@ -1,10 +1,17 @@
-package Задание1;
+package com.company;
+import java.util.Scanner;
 public class Main {
-    public static void main (String[] args) {
-        if (args.length !=0) {
-            System.out.println("Hello " + args[0]);
+
+    public static void main(String[] args) {
+    System.out.println("Введите имя");
+    Scanner str = new Scanner(System.in);
+    String name = str.nextLine();
+    if (name.equals(""))
+        {
+            System.out.println("Hello world");
         } else {
-            System.out.println("Hello " + "world");
-        }
+        System.out.println("Hello " + name);
+            }
+
     }
 }
